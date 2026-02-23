655d8dd681834fb2d361e22e2ebe474d440f302e
Usman Khan <imusmankhan10@gmail.com>
2026-02-23 12:53:38 +0500

    k

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './components/Navbar.css'
+import './components/Hero.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Steps from './components/Steps'
+import AppGallery from './components/AppGallery'
+import Testimonials from './components/Testimonials'
+import Footer from './components/Footer'
+
+function App() {
+    return (
+        <div className="app">
+            <Navbar />
+            <Hero />
+            <Features />
+            <Steps />
+            <AppGallery />
+            <Testimonials />
+            <Footer />
+        </div>
+    )
+}
+
+export default App
